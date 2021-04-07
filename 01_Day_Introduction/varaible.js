e97e7c50e8ee159b5023def6d0d7039c827abbcf
Harshal Raikwar <64153988+Harshal0902@users.noreply.github.com>
2021-04-07 22:49:52 +0530

    Add files via upload

---
+++ 01_Day_Introduction/varaible.js
@@ -0,0 +1,20 @@
+// Declaring different variables of different data types
+
+let firstName = 'Harshal'    // first name of a person
+let lastName = 'Raikwar'      // last name of a person
+let country = 'India'       // country
+let city = 'Bangalore'         // capital city
+let age = 100                 // age in years
+let isMarried = true
+
+// Declaring variables with number values
+
+const gravity = 9.81         // earth gravity  in m/s2
+const boilingPoint = 100     // water boiling point, temperature in oC
+const PI = 3.14              // geometrical constant
+
+// Variables can also be declaring in one line separated by comma
+
+let name = 'Harshal',     //name of a person
+  job = 'student',
+  live = 'India'
